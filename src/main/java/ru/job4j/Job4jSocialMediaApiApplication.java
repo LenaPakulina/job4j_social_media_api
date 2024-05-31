9c41eaa27c92f471ca19de5ad034b6de6c29bf0b
Elena Pakulina <el.pakulina@yandex.ru>
2024-05-31 20:40:09 +0500

    Добавила зависимости в pom xml 3

--- src/main/java/ru/job4j/Job4jSocialMediaApiApplication.java
+++ src/main/java/ru/job4j/Job4jSocialMediaApiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Job4jSocialMediaApiApplication {
 
 	public static void main(String[] args) {
+		System.out.println("test");
 		SpringApplication.run(Job4jSocialMediaApiApplication.class, args);
 	}
 
