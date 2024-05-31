f4e0d4ab841e320cdc15e93aff565ec523e75c5e
Elena Pakulina <el.pakulina@yandex.ru>
2024-05-31 23:02:40 +0500

    Рефакторинг. Убрала вывод в консоль

--- src/main/java/ru/job4j/Job4jSocialMediaApiApplication.java
+++ src/main/java/ru/job4j/Job4jSocialMediaApiApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Job4jSocialMediaApiApplication {
 
 	public static void main(String[] args) {
-		System.out.println("test");
 		SpringApplication.run(Job4jSocialMediaApiApplication.class, args);
 	}
 
