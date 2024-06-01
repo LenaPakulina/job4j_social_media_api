9375088295f0a4ff4ac74d40b378ccb27e0e700f
Elena Pakulina <el.pakulina@yandex.ru>
2024-06-01 15:35:42 +0500

    Именованные запросы [#505063 #498837]

--- src/main/java/ru/job4j/Job4jSocialMediaApiApplication.java
+++ src/main/java/ru/job4j/Job4jSocialMediaApiApplication.java
@@ -2,12 +2,23 @@ package ru.job4j;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import ru.job4j.repository.UserRepository;
 
 @SpringBootApplication
-public class Job4jSocialMediaApiApplication {
+@ComponentScan(basePackages = {"ru.job4j.repository"})
+public class Job4jSocialMediaApiApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Job4jSocialMediaApiApplication.class);
+	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(Job4jSocialMediaApiApplication.class, args);
+		var context = SpringApplication.run(Job4jSocialMediaApiApplication.class, args);
+		System.out.println(context.getBean(UserRepository.class));
 	}
 
 }
